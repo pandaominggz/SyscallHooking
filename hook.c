0c9956f5cc50ee4b7d4ad4bddff191bcfe428096
pandaoming <pandaominggz@qq.com>
2020-07-17 23:50:01 +0800

    address add

--- hook.c
+++ hook.c
@@ -4,8 +4,9 @@
 MODULE_LICENSE("GPL");
 char *sym_name = "sys_call_table";
 
+
 typedef asmlinkage long (*sys_call_ptr_t)(const struct pt_regs *);
-static sys_call_ptr_t *sys_call_table;
+static sys_call_ptr_t *_sys_call_table;
 
 
 sys_call_ptr_t old_open;
@@ -24,11 +25,12 @@ static asmlinkage long my_open(const struct pt_regs *regs)
 
 static int __init hello_init(void)
 {
-    sys_call_table = (sys_call_ptr_t *)kallsyms_lookup_name(sym_name);
-    old_open = sys_call_table[__NR_open];
+    unsigned long *sys_call_table = (unsigned long*) 0xffffffff938002a0;
+    _sys_call_table = (sys_call_ptr_t *)sys_call_table;
+    old_open = _sys_call_table[__NR_open];
     // Temporarily disable write protection
     write_cr0(read_cr0() & (~0x10000));
-    sys_call_table[__NR_open] = my_open;
+    _sys_call_table[__NR_open] = my_open;
     // Re-enable write protection
     write_cr0(read_cr0() | 0x10000);
 
@@ -39,7 +41,7 @@ static void __exit hello_exit(void)
 {
     // Temporarily disable write protection
     write_cr0(read_cr0() & (~0x10000));
-    sys_call_table[__NR_open] = old_open;
+    _sys_call_table[__NR_open] = old_open;
     // Re-enable write protection
     write_cr0(read_cr0() | 0x10000);
 
